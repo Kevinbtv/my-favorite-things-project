013cdfc27c95e2d5935a7f379942593708d6165f
kevinbtv <kevintavaresdev@gmail.com>
2023-09-01 02:35:45 -0300

    Estruturação inicial do frontend e dockeriza

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 set -e
 
-# Função para verificar se o banco de dados está pronto para conexões
-function wait_for_db() {
-    until nc -zv db 5432; do
-        echo "Aguardando o banco de dados..."
-        sleep 2
-    done
-}
-
-wait_for_db
-
-# Executar as migrações e a aplicação
-npx prisma migrate deploy --preview-feature
+npx prisma migrate dev
 npm run dev
